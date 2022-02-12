785592bee18ba1c3813d3c09bfef556464a6256c
Natalie Au <au16@my.yorku.ca>
2022-02-12 18:13:27 -0500

    Initial commit

---
+++ app/src/main/java/ellehack2022/frshair/MainActivity.java
@@ -0,0 +1,14 @@
+package ellehack2022.frshair;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
